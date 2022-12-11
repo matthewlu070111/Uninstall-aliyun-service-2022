c62fb550fbc5a06978a84197d49658fafaff4083
imxiaoanag <matthewlu070111@outlook.com>
2022-12-11 20:49:54 +0800

    1.0.0 Test

--- UAS.sh
+++ UAS.sh
@@ -13,10 +13,120 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # 赋予变量定义
 sh_ver="1.0.0"
+
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
+# 卸载云盾
+YunDun() {
+    # 创建程序执行环境
+    mkdir Uninstall-aliyun-service
+    chmod 777 Uninstall-aliyun-service
+    cd Uninstall-aliyun-service
+
+    # 为CentOS 6/7补充运行环境
+    yum install redhat-lsb -y
+
+    # 使用官方工具进行卸载
+    wget http://update.aegis.aliyun.com/download/uninstall.sh && chmod +x uninstall.sh && ./uninstall.sh
+    wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh && chmod +x quartz_uninstall.sh && ./quartz_uninstall.sh
+    
+    # 杀死阿里云服务后台程序
+    pkill aliyun-service
+    rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+    rm -rf /usr/local/aegis*
+    rm /usr/sbin/aliyun-service
+    rm /lib/systemd/system/aliyun.service
+
+    # 屏蔽阿里云云盾IP
+    iptables -I INPUT -s 140.205.201.0/28 -j DROP
+    iptables -I INPUT -s 140.205.201.16/29 -j DROP
+    iptables -I INPUT -s 140.205.201.32/28 -j DROP
+    iptables -I INPUT -s 140.205.225.192/29 -j DROP
+    iptables -I INPUT -s 140.205.225.200/30 -j DROP
+    iptables -I INPUT -s 140.205.225.184/29 -j DROP
+    iptables -I INPUT -s 140.205.225.183/32 -j DROP
+    iptables -I INPUT -s 140.205.225.206/32 -j DROP
+    iptables -I INPUT -s 140.205.225.205/32 -j DROP
+    iptables -I INPUT -s 140.205.225.195/32 -j DROP
+    iptables -I INPUT -s 140.205.225.204/32 -j DROP
+    iptables -I INPUT -s 106.11.222.0/23 -j DROP
+    iptables -I INPUT -s 106.11.224.0/24 -j DROP
+    iptables -I INPUT -s 106.11.228.0/22 -j DROP
+    service iptables save
+    
+    # 删除阿里云云盾剩余文件，进行欺骗伪装
+    rm -rf /etc/motd
+    touch /etc/motd
+
+    # 删除环境
+    cd ..
+    rm -rf Uninstall-aliyun-service
+}
+
+# 卸载云监控Go版本
+go() {
+    # 检测系统架构
+    os = $(uname -m)
+    if [ os == "x86_64" ]
+        export ARCH=amd64
+    else
+        export ARCH=386
+    fi
+
+    # 从系统服务中移除
+    /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} uninstall
+
+    # 停止
+    /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} stop
+
+    # 卸载
+    /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} stop && \
+    /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} uninstall && \
+    rm -rf /usr/local/cloudmonitor
+}
+
+# 轻量云服务器
+light_server() {
+    YunDun
+    clear
+
+    # 显示卸载完成
+    echo -e "${Info}: 卸载已完成" && exit 1
+}
+
+# 云服务器
+server() {
+    YunDun
+    go
+    clear
+
+    # 显示卸载完成
+    echo -e "${Info}: 卸载已完成" && exit 1
+}
+
+#更新脚本
+Update_Shell(){
+	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/matthewlu070111/Uninstall-aliyun-service-2022/main/UAS.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
+	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
+		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		read -p "(默认: y):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+			wget -N --no-check-certificate https://raw.githubusercontent.com/matthewlu070111/Uninstall-aliyun-service-2022/main/UAS.sh && chmod +x tcp.sh
+			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+		else
+			echo && echo "	已取消..." && echo
+		fi
+	else
+		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		sleep 5s
+	fi
+}
 
 # 开始菜单
 start_menu() {
@@ -31,12 +141,13 @@ start_menu() {
     read -p "请输入数字 [0-3]:" num
     case "$num" in
         0)
+        Update_Shell
         ;;
         1)
-        #light_server
+        light_server
         ;;
         2)
-        #server
+        server
         ;;
         3)
         exit 1
@@ -51,50 +162,3 @@ start_menu() {
 }
 
 start_menu
-
-mkdir Uninstall-aliyun-service
-chmod 777 Uninstall-aliyun-service
-cd Uninstall-aliyun-service
-
-clear
-
-yum install redhat-lsb -y
-
-wget http://update.aegis.aliyun.com/download/uninstall.sh
-chmod +x uninstall.sh
-./uninstall.sh
-wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
-chmod +x quartz_uninstall.sh
-./quartz_uninstall.sh
-pkill aliyun-service
-rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
-rm -rf /usr/local/aegis*
-rm /usr/sbin/aliyun-service
-rm /lib/systemd/system/aliyun.service
-iptables -I INPUT -s 140.205.201.0/28 -j DROP
-iptables -I INPUT -s 140.205.201.16/29 -j DROP
-iptables -I INPUT -s 140.205.201.32/28 -j DROP
-iptables -I INPUT -s 140.205.225.192/29 -j DROP
-iptables -I INPUT -s 140.205.225.200/30 -j DROP
-iptables -I INPUT -s 140.205.225.184/29 -j DROP
-iptables -I INPUT -s 140.205.225.183/32 -j DROP
-iptables -I INPUT -s 140.205.225.206/32 -j DROP
-iptables -I INPUT -s 140.205.225.205/32 -j DROP
-iptables -I INPUT -s 140.205.225.195/32 -j DROP
-iptables -I INPUT -s 140.205.225.204/32 -j DROP
-iptables -I INPUT -s 106.11.222.0/23 -j DROP
-iptables -I INPUT -s 106.11.224.0/24 -j DROP
-iptables -I INPUT -s 106.11.228.0/22 -j DROP
-service iptables save
-rm -rf /etc/motd
-touch /etc/motd
-cd -
-rm -rf Uninstall-aliyun-service
-clear
-
-printf "
-#######################################################################
-#                       Uninstall-aliyun-service                      #
-#                                 Done!                               #
-#######################################################################
-"
